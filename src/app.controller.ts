a092c484b4d2decc55d9b7b873b519fbd6ee755a
joao <joao-fischer@hotmail.com>
2025-03-06 18:19:47 -0300

    :sparkles: feat(prisma): implement initial Prisma setup with User and Product tables

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('app') //serve como rota principals
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
-  @Get()
+  @Get('teste')
   getHello(): string {
     return this.appService.getHello();
   }
