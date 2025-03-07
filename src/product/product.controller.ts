fce2e47e583383243ef78432aae73734a0df6482
joao <joao-fischer@hotmail.com>
2025-03-07 17:03:00 -0300

    :test_tube: feat(product): include IsPublic for testing

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -2,21 +2,26 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { ProductService } from './product.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { IsPublic } from 'src/auth/decorators/is-public.decorator';
 
 @Controller('product')
 export class ProductController {
-  constructor(private readonly productService: ProductService) {}
+  constructor(private readonly productService: ProductService) { }
 
-  @Post()
+
+  @Post('NewProduct')
   create(@Body() createProductDto: CreateProductDto) {
     return this.productService.create(createProductDto);
   }
 
-  @Get()
+  @IsPublic()
+  @Get('AllProducts')
   findAll() {
     return this.productService.findAll();
   }
 
+
+  @IsPublic()
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.productService.findOne(+id);
