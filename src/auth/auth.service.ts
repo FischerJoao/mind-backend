c105211180f427bfcfe8645c51d6d664ba7a7548
joao <joao-fischer@hotmail.com>
2025-03-07 12:17:43 -0300

    :sparkles: feat(auth): implement JWT authentication and enhance login functionality

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,32 @@
 import { Injectable } from "@nestjs/common";
 import { UserService } from "src/user/user.service";
 import * as bcrypt from "bcrypt";
+import { User } from "src/user/entities/user.entity";
+import { UserPayload } from "./models/UserPayload";
+import { JwtService } from "@nestjs/jwt";
+import { access } from "fs";
+import { UserToken } from "./models/UserToken";
 
 @Injectable()
 export class AuthService {
-    constructor(private readonly userService: UserService) { } // acessar servico pelo construtor injecao de dependencia
+    constructor(private readonly userService: UserService, private readonly jwtService: JwtService) { } // acessar servico pelo construtor injecao de dependencia
+
+
+    login(user: User): UserToken {
+
+        const paylod: UserPayload = {
+            sub: user.id,
+            email: user.email,
+            name: user.name,
+
+        }
+        const jwtToken = this.jwtService.sign(paylod);
+
+        return {
+            access_token: jwtToken,
+        }
+        //user vira jwt
+    }
 
     async validateUser(email: string, password: string) {
         const user = await this.userService.findOneByEmail(email);
