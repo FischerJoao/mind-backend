75f62bea2aba66a4b111521802ae9f3037d9fb53
joao <joao-fischer@hotmail.com>
2025-03-07 14:26:52 -0300

    :wrench: feat(app): add public route and current user retrieval endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,21 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { IsPublic } from './auth/decorators/is-public.decorator';
+import { User } from '@prisma/client';
+import { CurrentUser } from './auth/decorators/current-user.decorator';
 
 @Controller() //serve como rota principals
 export class AppController {
   constructor(private readonly appService: AppService) { }
 
+  @IsPublic()
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('me')
+  getMe(@CurrentUser() user: User) {
+    return user;
   }
 }
