7b3d20e60d878c13b4b5521e4726876512e7f109
joao <joao-fischer@hotmail.com>
2025-03-07 17:00:49 -0300

    🏷️ feat(product): enhance CreateProductDto with validation rules for properties

--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,1 +1,22 @@
-export class CreateProductDto {}
+import { IsString, IsOptional, IsInt, IsDecimal, Min, IsNumber } from 'class-validator';
+
+export class CreateProductDto {
+    @IsString()
+    name: string;
+
+    @IsString()
+    description: string;
+
+    @IsOptional()
+    @IsString()
+    imageUrl?: string;
+
+    // Usar "number" para garantir que é interpretado como número
+    @IsNumber()
+    @Min(0)
+    price: number; // Garantir que "price" seja um número (não uma string)
+
+    @IsInt()
+    @Min(0)
+    quantity: number;
+}
