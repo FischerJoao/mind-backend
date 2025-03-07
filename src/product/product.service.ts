461bc779c071da91087598be114cd0aeee2516b4
joao <joao-fischer@hotmail.com>
2025-03-07 16:59:27 -0300

    ✨ feat(product): integrate Prisma for product CRUD operations

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,19 +1,37 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class ProductService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  constructor(private readonly prisma: PrismaService) { }
+
+
+  async create(createProductDto: CreateProductDto) {
+
+    const data = {
+      ...createProductDto,
+    };
+
+    const createProduct = await this.prisma.product.create({
+      data
+    });
+
+    return {
+      ...createProduct,
+    }
   }
 
-  findAll() {
-    return `This action returns all product`;
+  async findAll() {
+    return await this.prisma.product.findMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: number) {
+    return await this.prisma.product.findUnique(
+      {
+        where: { id }
+      });
   }
 
   update(id: number, updateProductDto: UpdateProductDto) {
