5756716b63c353807017c654348b9846af72f273
joao <joao-fischer@hotmail.com>
2025-03-07 00:54:48 -0300

    :sparkles: feat(user): create user with hashed password

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,47 @@
-import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Injectable } from "@nestjs/common";
+import { CreateUserDto } from "./dto/create-user.dto";
+import { UpdateUserDto } from "./dto/update-user.dto";
+import { PrismaService } from "src/prisma/prisma.service";
+import * as bcrypt from "bcrypt";
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(private readonly prisma: PrismaService) { }
+
+  async create(createUserDto: CreateUserDto) {
+    //pegando as propriedades do createUserDto
+    //async pois tem que esperar o bcrypt 
+    const data = {
+      ...createUserDto,
+      password: await bcrypt.hash(createUserDto.password, 10)
+    };
+
+    //retorna promise
+    const createdUser = await this.prisma.user.create({
+      data
+    });
+
+    //pega o password e retorna undefined
+    return {
+      ...createdUser,
+      password: undefined
+    };
   }
 
-  findAll() {
-    return `This action returns all user`;
-  }
+  // findAll() {
+  //   return `This action returns all user`;
+  // }
 
   findOne(id: number) {
     return `This action returns a #${id} user`;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
+  // update(id: number, updateUserDto: UpdateUserDto) {
+  //   return `This action updates a #${id} user`;
+  // }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
-  }
+  // remove(id: number) {
+  //   return `This action removes a #${id} user`;
+  // }
 }
