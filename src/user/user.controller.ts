25cc6e5d9b047545764909bd0985b1b7f4444786
joao <joao-fischer@hotmail.com>
2025-03-07 18:22:45 -0300

    :wrench: feat(user): update create user route to 'newUser'

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,9 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete
+  Controller, Get, Post, Body, Patch, Param, Delete
 } from "@nestjs/common";
+
+
 import { UserService } from "./user.service";
 import { CreateUserDto } from "./dto/create-user.dto";
 import { UpdateUserDto } from "./dto/update-user.dto";
@@ -18,7 +14,7 @@ export class UserController {
   constructor(private readonly userService: UserService) { }
 
   @IsPublic()
-  @Post()
+  @Post('newUser')
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
   }
