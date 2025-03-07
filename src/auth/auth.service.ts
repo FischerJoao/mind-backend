78071252eb56718ae72cbf6b2837d188f251b77e
joao <joao-fischer@hotmail.com>
2025-03-07 11:19:56 -0300

    :sparkles: feat(auth): integrate UserService and implement user validation in AuthService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { UserService } from "src/user/user.service";
+import * as bcrypt from "bcrypt";
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private readonly userService: UserService) { } // acessar servico pelo construtor injecao de dependencia
+
+    async validateUser(email: string, password: string) {
+        const user = await this.userService.findOneByEmail(email);
+
+        if (user) {
+            //comparar senha
+
+            const isPasswordValid = await bcrypt.compare(password, user.password);
+
+            if (isPasswordValid) {
+                return {
+                    ...user,
+                    password: undefined,
+                };
+            }
+        }
+
+        //nao encontrou nada
+        throw new Error("Incorrect email or password");
+    }
+}
