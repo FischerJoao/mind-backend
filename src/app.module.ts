dc7dacc02a10d4fcb3b096ffaa77e54bec4bf467
joao <joao-fischer@hotmail.com>
2025-03-09 17:30:48 -0300

    🔧feat(app): add ServeStaticModule to serve uploads directory

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,16 +7,29 @@ import { AuthModule } from './auth/auth.module';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 import { ProductModule } from './product/product.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+
 
 
 @Module({
-  imports: [PrismaModule, UserModule, AuthModule, ProductModule],
+  imports: [
+    PrismaModule,
+    UserModule,
+    AuthModule,
+    ProductModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'uploads'),
+      serveRoot: '/uploads',
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService,
+  providers: [
+    AppService,
     {
       provide: APP_GUARD,
       useClass: JwtAuthGuard,
-    }
+    },
   ],
 })
 export class AppModule { }
